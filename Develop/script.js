66a654ca6cb2acf917852a5add948fe84f9afa5d
Adam Alcantara <adamjalcantara@gmail.com>
2021-04-02 22:09:28 -0400

    Added all of the comments

--- Develop/script.js
+++ Develop/script.js
@@ -7,10 +7,11 @@ $(document).ready(function () {
         console.log(textValue);
         console.log(textTime);
 
+        //put the items into localStorage
         localStorage.setItem(textValue, textTime);
     })
 
-})
+
 
 //Use moment to put the time on the page
 var today = moment();
@@ -22,19 +23,27 @@ function currentTime() {
     var currentHour = moment().hour();
     console.log(currentHour);
 
+    //get variables for each item with a class of "time-blocks"
     $(".time-blocks").each(function () {
+        //Get the id attribute of this (this being the rows with the class of time blocks)
         var blockHour = ($(this).attr('id'));
 
+        //add the past class if the block hour is less than the current time
         if (blockHour < currentHour) {
             $(this).addClass("past");
         }
+        //add the present class if the block hour is equal to the current hour
         if (blockHour === currentHour) {
             $(this).addClass("present");
         }
+        //add the future class if the block hour is more than the current hour
         if (blockHour > currentHour) {
             $(this).addClass("future");
         }
     })
 }
 
+//call the currentTime function so that it runs
 currentTime();
+
+})
